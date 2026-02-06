5b346706edea94c23b638da26ff969f49a667793
David Miles <david@digbyswift.com>
2026-02-06 16:39:58 +0000

    Surpress warning due to MockHeaderCollection nullability

--- src/Digbyswift.Http.Tests/MockObjects/MockHeaderCollection.cs
+++ src/Digbyswift.Http.Tests/MockObjects/MockHeaderCollection.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Primitives;
 
 namespace Digbyswift.Extensions.Http.Tests.MockObjects;
 
+#pragma warning disable CS8644 // Type does not implement interface member. Nullability of reference types in interface implemented by the base type doesn't match.
 public class MockHeaderCollection : Dictionary<string, StringValues>, IHeaderDictionary
 {
     public long? ContentLength { get; set; }
